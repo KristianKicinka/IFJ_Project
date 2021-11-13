ab8a2c8f831ad4d8f529e349322359dc72dd14bf
pindo696 <pindo696@outlook.com>
2021-11-13 20:04:39 +0100

    uploaded basic tests for stack

---
+++ tests/stack_test.c
@@ -0,0 +1,29 @@
+#include "../stack.h"
+#include "../stack.c"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+
+int main()
+{/*
+    Stack_symbol_t stack;
+    printf("STACK TEST\n");
+    Stack_Init(&stack);
+    for(int i=0, i<100, i++){
+        int c=Stack_Push(&stack, i, 'N');
+        if(c==FALSE){
+             printf("Malloc vratil false\n");
+             break;
+        }
+    }
+    
+    for (int f = 0; f < 100; i++){
+        char symbol=stack.top->stack_symbol;
+    }
+    */
+  
+    
+    return 0;
+}
