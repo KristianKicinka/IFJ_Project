41da8a89c578160b047626f56c092e72504fd511
KristianKicinka <kristian.kicinka@gmail.com>
2021-11-15 00:38:40 +0100

    After add some functions to scanner and error (not tested yet)

--- error.h
+++ error.h
@@ -2,17 +2,21 @@
 #define ERROR_H
 
 typedef enum {
-    SCANNER_ANALYSIS_FAIL,
-    SCANNER_ANALYSIS_SUCCESS,
-    SYNTAX_ANALYSIS_FAIL,
-    SYNTAX_ANALYSIS_SUCCESS,
-    SEMANTIC_ANALYSIS_UNDEF_VAR,
-    SEMANTIC_ANALYSIS_UNCOMPATIBILE_TYPE,
-    SEMANTIC_ANALYSIS_FAIL,
-    INTERNAL_FAILATURE,
+    SCANNER_ANALYSIS_FAIL,                                                     // 1
+    SCANNER_ANALYSIS_SUCCESS,                                                  // 0
+    SYNTAX_ANALYSIS_FAIL,                                                      // 2
+    SYNTAX_ANALYSIS_SUCCESS,                                                   // 0
+    SEMANTIC_ANALYSIS_UNDEF_VAR,                                               // 3
+    SEMANTIC_ANALYSIS_UNCOMPATIBILE_TYPE_ASSIGN,                               // 4
+    SEMANTIC_ANALYSIS_BAD_COUNT_PARAMETERS,                                    // 5
+    SEMANTIC_ANALYSIS_UNCOMPATIBILE_TYPE_ARITMETIC,                            // 6                   
+    SEMANTIC_ANALYSIS_FAIL_OTHERS,                                             // 7
+    RUNTIME_ERROR_WITH_NIL,                                                    // 8
+    RUNTIME_ERROR_DIVIDING_BY_ZERO,                                            // 9
+    INTERNAL_FAILATURE,                                                        // 99
 
 }Error_type;
 
-void process_error(Error_type *err_type);
+int process_error(Error_type *err_type);
 
 #endif // !ERROR_H
