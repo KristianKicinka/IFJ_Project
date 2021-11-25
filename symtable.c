d58ac5ebda2a0e3708d92c795e622145f6bd9aef
KristianKicinka <kristian.kicinka@gmail.com>
2021-11-25 23:43:07 +0100

    after add some stuff

--- symtable.c
+++ symtable.c
@@ -24,6 +24,62 @@ int get_hash(char *key) {
 }
 
 
+void table_init(symbol_table_t *table){
+
+  for (int index = 0; index < HT_SIZE; index++ ){
+      (*table)[index] = NULL;
+    }
+}
+
+
+table_item_t *search(table_symbol_t *table, char *key){
+
+  int hash_index = get_hash(key);                                              
+  table_item_t *temp_item = (*table)[hash_index];                                
+
+  while (temp_item != NULL){                                                   
+
+    if(strcmp(temp_item->key,key) == 0){
+      return temp_item;  
+    }else{
+      temp_item = temp_item->next;
+    }
+                       
+  }
+
+  return NULL;
+}
+
+void insert_symbol_variable(symbol_table_t *table, char *key , char *id, Symbol_type type, Additional_info *info){
+
+  table_item_t *new_item = malloc(sizeof(table_item_t));
+  if(new_item == NULL)
+    return;
+
+  new_item->key = malloc(sizeof(char)*(strlen(key)+1));
+  if(new_item->key == NULL)
+    return;
+  
+  int new_index = get_hash(key);
+  table_item_t *found_item = ht_search(table,key);
+  
+  strcpy(new_item->key,key);
+
+  //TODO
+  
+  if(found_item != NULL){
+    found_item->value = value;
+    free(new_item->key);
+    free(new_item);
+
+  }else{
+    new_item->next = (*table)[new_index];
+    (*table)[new_index] = new_item;
+  }
+
+}
+
+
 
 
 
