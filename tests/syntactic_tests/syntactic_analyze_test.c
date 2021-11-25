f5c443706cd150c7d71277f167a9025f5937fd68
pindo696 <pindo696@outlook.com>
2021-11-25 23:39:18 +0100

    "bone" for grammar check added

--- tests/syntactic_tests/syntactic_analyze_test.c
+++ tests/syntactic_tests/syntactic_analyze_test.c
@@ -3,17 +3,16 @@
 #include "../../LL_table.c"
 
 int main(){
+    
+    
 
-    Stack_symbol_t stack;
-    Stack_Init(&stack);
-    Stack_Push(&stack, FALSE, NULL, NT_PROLOG);
-    Token token;
-    token.type_of_token=TYPE_KW_REQUIRE;
-    CheckGrammar(&token, &stack);
 
+
+   /*
+   REVERSE RULES
     for(int i=number_of_derivations-1, i>=0, i--){
         push();
-    }
+    }*/
 
 
     Stack_Free(&stack);
