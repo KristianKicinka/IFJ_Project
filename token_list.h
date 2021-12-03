bf26a92d0863c81d8b2356299b03f2d20960d3f8
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-03 18:06:40 +0100

    after add functions to token_list.h

--- token_list.h
+++ token_list.h
@@ -14,15 +14,23 @@
 
 #include "lexical_analysis.h"
 
-typedef struct Token_list_t{
+typedef struct Token_list_element_t{
     Token *token;
     Token_list_t *next;
+}Token_list_element;
+
+typedef struct Token_list_t{
+    Token_list_element *head;
 }Token_list;
 
 void token_list_init(Token_list *token_list);
 
 void token_list_insert(Token_list *token_list, Token *token);
 
-void get_
+Token *token_list_get_token(Token_list *token_list);
+
+bool token_list_is_empty(Token_list *token_list);
+
+void token_list_dispose(Token_list *token_list);
 
 #endif // !TOKEN_LIST_H
