a1b11626c20a816a97df77f9319dd9b9e9619878
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-03 11:15:50 +0100

    commit

---
+++ token_list.c
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Project : Implementace překladače imperativního jazyka IFJ21.
+ * 
+ * @file token_list.c 
+ * @author Kristián Kičinka (xkicin02)
+ * @brief
+ * 
+ */
+
+#include "token_list.h"
+
+//TODO
