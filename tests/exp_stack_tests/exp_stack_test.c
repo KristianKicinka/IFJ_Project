d27e0d814ad7b5e304f1f3db7c65135756a787c0
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-03 02:45:03 +0100

    after add tests for expression stack

--- tests/exp_stack_tests/exp_stack_test.c
+++ tests/exp_stack_tests/exp_stack_test.c
@@ -2,25 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_stack_items(Exp_stack_item *item);
 
 int main(){
-//    Stack_symbol_t stack;
-//   // Stack_t *tmp;  
-//
-//    Stack_Init (&stack);    
-//
-//    test_stack_empty(&stack);
-//
-//    test_stack_insert(&stack, -42, NT_ARGUMENT, false);
-//    test_stack_insert(&stack, TYPE_KW_WHILE, -42, true);
-//
-//    test_stack_empty(&stack);
-//    
-//    test_till_top_terminal(&stack);
-//
-//    test_stack_print(&stack);
-//
-//    Stack_Free(&stack);
+
+    Exp_stack_symbol stack;
+    Exp_stack_symbol *my_stack = &stack;
+    
+    exp_stack_init (my_stack);    
+
+    bool err = exp_stack_push(my_stack,PLUS,TYPE_INT_NUMBER);
+    bool err_3 = exp_stack_push(my_stack,PLUS,TYPE_STRING);
+
+    if(err == false || err_3 == false )
+        return 0;
+    
+    print_stack_items(exp_stack_top(my_stack));
+    print_stack_items(exp_stack_top(my_stack)->next);
+
+    bool err2 = exp_stack_free(my_stack);
+
+    if(err2 == false)
+        return 0;
 
     return 0;
+}
+
+void print_stack_items(Exp_stack_item *item){
+    printf("#######################\n");
+    printf("symbol : %d \n",item->symbol);
+    printf("type : %d \n",item->type);
+    printf("#######################\n");
 }
