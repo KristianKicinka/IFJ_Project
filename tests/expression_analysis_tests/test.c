e3996a50aff0a8045facf7b75bd6bfe1ded7ab01
MajerMiso <majerko88@gmail.com>
2021-12-06 19:23:50 +0100

    Fixed valgrind

--- tests/expression_analysis_tests/test.c
+++ tests/expression_analysis_tests/test.c
@@ -1,9 +1,26 @@
+#include "../../token_list.h"
+#include "../../lexical_analysis.h"
+#include "../../recursive_syntactic.h"
+#include "../../error.h"
+#include "../../expression_analysis.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "../../expression_analysis.h"
+
+void test()
+{
+}
 
 int main()
 {
-    // TODO
+    int err = 0;
+    int i = 0;
+
+    while (!feof(stdin) && err == 0)
+    {
+        i++;
+        printf("while cislo %d \n", i);
+
+        test();
+    }
     return 0;
 }
